289fb074b61d20e5b940a219440c9da99a6cb432
Rubén López Casas <rlopez@nts-solutions.com>
2021-03-03 19:14:41 +0100

    feat: componentes lwc para botones de grabar y encuesta

---
+++ force-app/zurich-base/main/default/lwc/cTIExtraButtons/cTIExtraButtons.js
@@ -0,0 +1,8 @@
+import { LightningElement } from "lwc";
+import extraButtonsTitle from "@salesforce/label/c.CTIExtraButtonsTitle";
+
+export default class CTIExtraButtons extends LightningElement {
+  label = {
+    extraButtonsTitle
+  };
+}
---
+++ force-app/zurich-base/main/default/lwc/callRecordingButton/callRecordingButton.js
@@ -0,0 +1,46 @@
+import { LightningElement, api, track } from "lwc";
+import callRecordingStart from "@salesforce/label/c.callRecordingButtonStart";
+import callRecordingStop from "@salesforce/label/c.callRecordingButtonStop";
+
+export default class CallRecordingButton extends LightningElement {
+  label = {
+    callRecordingStart,
+    callRecordingStop
+  };
+
+  isSelected = false;
+  @track timeVal = "00:00";
+  timeIntervalInstance;
+  totalMilliseconds = 0;
+
+  handleClick() {
+    this.isSelected = !this.isSelected;
+    this.template.querySelector(`[data-id="record-button"]`).blur();
+
+    if (this.isSelected) {
+      //start
+      var parentThis = this;
+      this.timeIntervalInstance = setInterval(function () {
+        // Time calculations for hours, minutes, seconds and milliseconds
+        var minutes = Math.floor(
+          (parentThis.totalMilliseconds % (1000 * 60 * 60)) / (1000 * 60)
+        );
+        var seconds = Math.floor(
+          (parentThis.totalMilliseconds % (1000 * 60)) / 1000
+        );
+
+        // Output the result in the timeVal variable
+        parentThis.timeVal =
+          (minutes + "").padStart(2, "0") +
+          ":" +
+          (seconds + "").padStart(2, "0");
+
+        parentThis.totalMilliseconds += 100;
+      }, 100);
+    } else {
+      this.timeVal = "00:00";
+      this.totalMilliseconds = 0;
+      clearInterval(this.timeIntervalInstance);
+    }
+  }
+}
