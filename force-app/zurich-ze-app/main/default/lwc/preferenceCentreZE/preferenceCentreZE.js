2161307aa6bcbb5e6aa341e0a1def235f01fad14
agonzalezisasi <agonzalezisasi@nts-solutions.com>
2021-03-03 17:26:49 +0100

    feat: ocs 1286 - Centro Preferencias Logo Orange

--- force-app/zurich-ze-app/main/default/lwc/preferenceCentreZE/preferenceCentreZE.js
+++ force-app/zurich-ze-app/main/default/lwc/preferenceCentreZE/preferenceCentreZE.js
@@ -1,13 +1,14 @@
 import { LightningElement, api, track, wire } from "lwc";
 import getLabels from "@salesforce/apex/PreferenceCentreZEController.getLabels";
 import processRequest from "@salesforce/apex/PreferenceCentreZEController.processRequest";
-import ZURICH_LOGO from '@salesforce/resourceUrl/zurich_ze_logo';
- 
+import getLogoUrl from "@salesforce/apex/PreferenceCentreZEController.getLogoUrl";
+
 export default class preferenceCentre extends LightningElement {
   // expose the static resource url for use in the template
-  zurichLogoUrl = ZURICH_LOGO;
+  @track zurichLogoUrl = "";
 
   @api hash;
+  @api scope;
   @track scopeHash;
   @track _isCheckedHasOptedOutOfEmail = false;
   @track clickedButtonLabel;
@@ -23,44 +24,50 @@ export default class preferenceCentre extends LightningElement {
     PreferenceCentreRedirect: ""
   };
 
-  @wire(getLabels) 
+  // Get the logo url by the scope parameter
+  @wire(getLogoUrl, { scope: "$scope" }) wiredLogoResult(result) {
+    if (result.data) {
+      this.zurichLogoUrl = result.data;
+    }
+  }
+
+  @wire(getLabels)
   getLabels({ error, data }) {
-      if (data) {
-          this.label = JSON.parse(data);
-      } else if (error) {
-          console.error("Error recuperando etiquetas: " + JSON.stringify(error));
-      }
+    if (data) {
+      this.label = JSON.parse(data);
+    } else if (error) {
+      console.error("Error recuperando etiquetas: " + JSON.stringify(error));
+    }
   }
 
   connectedCallback() {
-      this.scopeHash = this.hash;
+    this.scopeHash = this.hash;
   }
 
   /*
     Event: user clicks Out of Email checkbox
   */
   handleChange(event) {
-      this._isCheckedHasOptedOutOfEmail = event.target.checked;
+    this._isCheckedHasOptedOutOfEmail = event.target.checked;
   }
 
   /*
     Event: user clicks save changes button
   */
   handleClick(event) {
-      this.clickedButtonLabel = event.target.label;
+    this.clickedButtonLabel = event.target.label;
 
-      try {
-        processRequest({
-            hashedId: this.scopeHash,
-            hasOptedOutOfEmail: this._isCheckedHasOptedOutOfEmail
-        });
+    try {
+      processRequest({
+        hashedId: this.scopeHash,
+        hasOptedOutOfEmail: this._isCheckedHasOptedOutOfEmail
+      });
 
-        if( this.label.PreferenceCentreRedirect !== '' ) {
-            window.location.replace(this.label.PreferenceCentreRedirect);
-        }
-        
-      } catch(error) {
-          console.error(error);
+      if (this.label.PreferenceCentreRedirect !== "") {
+        window.location.replace(this.label.PreferenceCentreRedirect);
       }
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
