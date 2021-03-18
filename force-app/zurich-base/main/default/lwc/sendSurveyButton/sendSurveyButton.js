1d26e160b42000bcf79bbbdb52f86301eb3b2734
Rubén López Casas <rlopez@nts-solutions.com>
2021-03-18 13:05:25 +0100

    feat: transferencia encuesta

--- force-app/zurich-base/main/default/lwc/sendSurveyButton/sendSurveyButton.js
+++ force-app/zurich-base/main/default/lwc/sendSurveyButton/sendSurveyButton.js
@@ -1,8 +1,10 @@
-import { LightningElement } from "lwc";
+import { LightningElement, track } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import sendSurveyLabel from "@salesforce/label/c.sendSurveyButtonLabel";
 import sendSurveyToastTitleSuccess from "@salesforce/label/c.sendSurveyToastTitleSuccess";
 import sendSurveyToastMessageSuccess from "@salesforce/label/c.sendSurveyToastMessageSuccess";
+import genesysCloud from "c/genesysCloudService";
+import getPollPhoneNumber from "@salesforce/apex/SendSurveyButtonController.getTransferPollPhoneNumber";
 
 export default class SendSurveyButton extends LightningElement {
   label = {
@@ -11,7 +13,22 @@ export default class SendSurveyButton extends LightningElement {
     sendSurveyToastMessageSuccess
   };
 
+  @track isDisabled = true;
+  pollPhoneNumber;
+
+  @track logs = [];
+  connectedCallback() {
+    getPollPhoneNumber().then((result) => {
+      if (result) {
+        this.isDisabled = false;
+      }
+
+      this.pollPhoneNumber = result;
+    });
+  }
+
   sendSurvey() {
+    genesysCloud.transfer(this.pollPhoneNumber);
     const event = new ShowToastEvent({
       title: this.label.sendSurveyToastTitleSuccess,
       message: this.label.sendSurveyToastMessageSuccess,
