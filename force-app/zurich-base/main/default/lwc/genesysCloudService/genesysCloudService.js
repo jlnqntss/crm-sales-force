2527e207dacd2018f41ec7eaf13b8edbdb320702
Javier Martínez de Pissón <jmartinezpisson@nts-solutions.com>
2021-03-31 12:07:53 +0200

    feat: librería de API de usuario de Genesys Cloud
    Se implementa librería JS y Apex que permite hacer uso de la API de Genesys Cloud para conferenciar y recuperar llamadas activas

---
+++ force-app/zurich-base/main/default/lwc/genesysCloudService/genesysCloudService.js
@@ -0,0 +1,87 @@
+import {
+  APPLICATION_SCOPE,
+  createMessageContext,
+  publish,
+  subscribe
+} from "lightning/messageService";
+import genesysCloudChannel from "@salesforce/messageChannel/purecloud__ClientEvent__c";
+import conferenceTo from "@salesforce/apex/GenesysCloudLightningController.conferenceTo";
+import isAuthorized from "@salesforce/apex/GenesysCloudLightningController.isAuthorized";
+import authorize from "@salesforce/apex/GenesysCloudLightningController.authorize";
+
+const messageContext = createMessageContext();
+const state = {
+  logs: [],
+  listeners: [],
+  currentInteractionId: null
+};
+
+subscribe(
+  messageContext,
+  genesysCloudChannel,
+  (message) => {
+    console.log(message);
+    state.logs.push(message);
+    if (message.type === "Interaction" && message.data.id) {
+      state.currentInteractionId = message.data.id;
+    }
+    if (state.listeners.length) {
+      state.listeners.forEach((listener) => {
+        listener(message);
+      });
+    }
+  },
+  { scope: APPLICATION_SCOPE }
+);
+
+function publishMessage(payload) {
+  return publish(messageContext, genesysCloudChannel, payload);
+}
+
+export default {
+  addListener(listener) {
+    state.listeners.push(listener);
+
+    return state.logs;
+  },
+  transfer(phoneNumber) {
+    return publishMessage({
+      type: "PureCloud.Interaction.updateState",
+      data: {
+        action: "blindTransfer",
+        id: state.currentInteractionId,
+        participantContext: {
+          transferTarget: encodeURIComponent(phoneNumber),
+          transferTargetType: "address"
+        }
+      }
+    });
+  },
+  consult(phoneNumber) {
+    return publishMessage({
+      type: "PureCloud.Interaction.updateState",
+      data: {
+        action: "consultTransfer",
+        id: state.currentInteractionId,
+        participantContext: {
+          transferTarget: encodeURIComponent(phoneNumber),
+          transferTargetType: "address"
+        }
+      }
+    });
+  },
+  conference(phoneNumber, uuiData) {
+    return conferenceTo({
+      toAddress: phoneNumber,
+      uuiData: uuiData
+    });
+  },
+  isAuthorized() {
+    return isAuthorized();
+  },
+  async authorize() {
+    var authorizeURL = await authorize();
+
+    window.open(authorizeURL);
+  }
+};
