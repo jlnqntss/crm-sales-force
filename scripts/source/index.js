a860b168c066c71831d0a62ad79b6ced185fac1c
isanjose <isanjose@nts-solutions.com>
2022-05-25 14:43:47 +0200

    fix: Update package node-find-folder

--- scripts/source/index.js
+++ scripts/source/index.js
@@ -2,16 +2,16 @@ const { readFileSync, writeFileSync } = require("fs");
 const SCRIPTS_DIR = process.env["CI_SCRIPTS_DIR"] + "/scripts/source";
 
 const scripts = {
-  "release:authenticate": `node ${SCRIPTS_DIR}/authenticate`,
-  "release:scan": `node ${SCRIPTS_DIR}/scan`,
-  "release:test:lwc": `node ${SCRIPTS_DIR}/test-lwc`,
-  "release:test:apex": `node ${SCRIPTS_DIR}/test-apex`,
-  "release:validate": `node ${SCRIPTS_DIR}/deploy --check`,
-  "release:deploy": `node ${SCRIPTS_DIR}/deploy`,
-  "release:changelog": `node ${SCRIPTS_DIR}/changelog`,
-  "release:tag:version": `node ${SCRIPTS_DIR}/tag-version`,
-  "release:start": `node ${SCRIPTS_DIR}/create-release`,
-  "release:finish": `node ${SCRIPTS_DIR}/finish-release`
+  "release:authenticate": `node ${SCRIPTS_DIR}/authenticate.js`,
+  "release:scan": `node ${SCRIPTS_DIR}/scan.js`,
+  "release:test:lwc": `node ${SCRIPTS_DIR}/test-lwc.js`,
+  "release:test:apex": `node ${SCRIPTS_DIR}/test-apex.js`,
+  "release:validate": `node ${SCRIPTS_DIR}/deploy.js --check`,
+  "release:deploy": `node ${SCRIPTS_DIR}/deploy.js`,
+  "release:changelog": `node ${SCRIPTS_DIR}/changelog.js`,
+  "release:tag:version": `node ${SCRIPTS_DIR}/tag-version.js`,
+  "release:start": `node ${SCRIPTS_DIR}/create-release.js`,
+  "release:finish": `node ${SCRIPTS_DIR}/finish-release.js`
 };
 /**
  * Inicializa el package.json para implementar los scripts de release automático
