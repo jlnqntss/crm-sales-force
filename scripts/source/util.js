d661efdecb29e1b0b015724259cc71c0cd1174a6
Olalla Iglesias <o.iglesiasalcala@zurich.com>
2022-06-08 08:41:13 +0000

    Revert "Merge branch 'feat-pipeline-scripts-update' into 'dev'"
    This reverts merge request !1213

--- scripts/source/util.js
+++ scripts/source/util.js
@@ -281,10 +281,7 @@ async function findLastSemanticTag(targetSuffix) {
 
   // 1 - Se obtienen las etiquetas de la referencia
   let currentBranchTags = await gitLabService.getTags();
-  console.log("[INFO] - Current Branch Tags");
-  console.log(currentBranchTags);
-  console.log("Target Suffix");
-  console.log(targetSuffix);
+
   // 2 - Se define la expresión regular de búsqueda
   // 2 - Se busca a través de expresión regular la etiqueta de versionado semántico con el sufijo de tipo
   let tagToSearch = new RegExp(
