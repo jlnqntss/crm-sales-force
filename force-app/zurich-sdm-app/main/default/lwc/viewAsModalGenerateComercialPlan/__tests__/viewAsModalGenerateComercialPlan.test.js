753b39b714e0000e4300d6ed543755bacf1d0d1e
damumo <dmunoz@albirasolutions.com>
2023-04-13 13:27:38 +0200

    feat: SDM-1403 correccion sonar eliminar comentario

--- force-app/zurich-sdm-app/main/default/lwc/viewAsModalGenerateComercialPlan/__tests__/viewAsModalGenerateComercialPlan.test.js
+++ force-app/zurich-sdm-app/main/default/lwc/viewAsModalGenerateComercialPlan/__tests__/viewAsModalGenerateComercialPlan.test.js
@@ -19,7 +19,6 @@ describe("c-view-as-modal-generate-comercial-plan", () => {
     document.body.appendChild(element);
 
     // Assert
-    // const div = element.shadowRoot.querySelector('div');
     expect(1).toBe(1);
   });
 });
