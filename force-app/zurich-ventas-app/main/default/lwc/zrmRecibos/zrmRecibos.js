e685beec8108c0b4b0f333cb725e016ebe6731d8
David Muñoz Moya <david.munoz@seidor.com>
2024-05-30 12:47:22 +0200

    feat: CV-2080 añadir custom labels al lwc, contador de pagina y traducciones

--- force-app/zurich-ventas-app/main/default/lwc/zrmRecibos/zrmRecibos.js
+++ force-app/zurich-ventas-app/main/default/lwc/zrmRecibos/zrmRecibos.js
@@ -2,99 +2,17 @@ import { LightningElement, api } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getData from '@salesforce/apex/ZrmRecibosController.getData';
 
-// Custom Labels
-import ZRM_Recibos_Previous from "@salesforce/label/c.ZRM_Recibos_Previous";
-import ZRM_Recibos_Next from "@salesforce/label/c.ZRM_Recibos_Next";
-import ZRM_Recibos_Column_Receipt from "@salesforce/label/c.ZRM_Recibos_Column_Receipt";
-import ZRM_Recibos_Column_EffectiveDate from "@salesforce/label/c.ZRM_Recibos_Column_EffectiveDate";
-import ZRM_Recibos_Column_Policy from "@salesforce/label/c.ZRM_Recibos_Column_Policy";
-import ZRM_Recibos_Column_Company from "@salesforce/label/c.ZRM_Recibos_Column_Company";
-import ZRM_Recibos_Column_Holder from "@salesforce/label/c.ZRM_Recibos_Column_Holder";
-import ZRM_Recibos_Column_Phone from "@salesforce/label/c.ZRM_Recibos_Column_Phone";
-import ZRM_Recibos_Column_Amount from "@salesforce/label/c.ZRM_Recibos_Column_Amount";
-import ZRM_Recibos_Column_PaymentMethod from "@salesforce/label/c.ZRM_Recibos_Column_PaymentMethod";
-import ZRM_Recibos_Column_NumberOfReturns from "@salesforce/label/c.ZRM_Recibos_Column_NumberOfReturns";
-import ZRM_Recibos_Column_DocumentDescriptionReceipt from "@salesforce/label/c.ZRM_Recibos_Column_DocumentDescriptionReceipt";
-import ZRM_Recibos_Column_LastReturnDate from "@salesforce/label/c.ZRM_Recibos_Column_LastReturnDate";
-import ZRM_Recibos_Column_BankShipmentDate from "@salesforce/label/c.ZRM_Recibos_Column_BankShipmentDate";
-import ZRM_Recibos_Column_Intermediary from "@salesforce/label/c.ZRM_Recibos_Column_Intermediary";
+// The rest of the relative imports
+import labels from "./labels";
+import columns from "./columns";
 
 export default class ZrmRecibos extends LightningElement {
 
-    // Variables js
-    labels = {
-        ZRM_Recibos_Previous,
-        ZRM_Recibos_Next,
-        ZRM_Recibos_Column_Receipt,
-        ZRM_Recibos_Column_EffectiveDate,
-        ZRM_Recibos_Column_Policy,
-        ZRM_Recibos_Column_Company,
-        ZRM_Recibos_Column_Holder,
-        ZRM_Recibos_Column_Phone,
-        ZRM_Recibos_Column_Amount,
-        ZRM_Recibos_Column_PaymentMethod,
-        ZRM_Recibos_Column_NumberOfReturns,
-        ZRM_Recibos_Column_DocumentDescriptionReceipt,
-        ZRM_Recibos_Column_LastReturnDate,
-        ZRM_Recibos_Column_BankShipmentDate,
-        ZRM_Recibos_Column_Intermediary
-    }
-
-    // columnas de la tabla
-    columns = [
-        { label: this.labels.ZRM_Recibos_Column_Receipt, fieldName: 'codigoRecibo', wrapText: true },
-        {   label: this.labels.ZRM_Recibos_Column_EffectiveDate, 
-            fieldName: 'fechaEfectoMovimiento', 
-            type: 'date',
-            typeAttributes: {
-                day: "2-digit",
-                month: "2-digit",
-                year: "numeric"
-            },
-            wrapText: true
-        },
-        { label: this.labels.ZRM_Recibos_Column_Policy, fieldName: 'codigoPoliza', wrapText: true },
-        { label: this.labels.ZRM_Recibos_Column_Company, fieldName: 'descripcionAgrupacionCompania', wrapText: true },
-        { label: this.labels.ZRM_Recibos_Column_Holder, fieldName: 'nombreTomador', wrapText: true },
-        { label: this.labels.ZRM_Recibos_Column_Phone, fieldName: 'telefonoTomador', wrapText: true },
-        {   label: this.labels.ZRM_Recibos_Column_Amount, 
-            fieldName: 'importeTotalRecibo', 
-            type: 'currency',
-            typeAttributes: { 
-                currencyCode: 'EUR', 
-                step: '0.001' 
-            },
-            wrapText: true
-        },
-        { label: this.labels.ZRM_Recibos_Column_PaymentMethod, fieldName: 'descripcionFormaPago', wrapText: true },
-        { label: this.labels.ZRM_Recibos_Column_NumberOfReturns, fieldName: 'numeroDevolucionesRecibo', wrapText: true },
-        { label: this.labels.ZRM_Recibos_Column_DocumentDescriptionReceipt, fieldName: 'descripcionDocumentoReclamacion', wrapText: true },
-        { 
-            label: this.labels.ZRM_Recibos_Column_LastReturnDate, 
-            fieldName: 'fechaUltimaDevolucionRecibo', 
-            type: 'date',
-            typeAttributes: {
-                day: "2-digit",
-                month: "2-digit",
-                year: "numeric"
-            },
-            wrapText: true
-        },
-        {   label: this.labels.ZRM_Recibos_Column_BankShipmentDate, 
-            fieldName: 'fechaEnvioBancoRecibo', 
-            type: 'date',
-            typeAttributes:{
-                day: "2-digit",
-                month: "2-digit",
-                year: "numeric"
-            },
-            wrapText: true
-        },
-        { label: this.labels.ZRM_Recibos_Column_Intermediary, fieldName: 'codigoIntermediario', wrapText: true }
-    ];
     
     @api antiguedadRecibo;
-
+    
+    labels = labels;
+    columns = columns;
     data = [];
     disablePreviousButton = true; 
     disableNextButton = true;
