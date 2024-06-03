5d337b4ad85dd2cef672ca610983711cea285fbc
David Muñoz Moya <david.munoz@seidor.com>
2024-06-03 10:23:57 +0200

    feat: CV-2437 modificado componenet ficheros de zrm para añadir el boton borrar para aquellos donde sea owner el usuario logueado

--- force-app/zurich-ventas-app/main/default/lwc/zrmFiles/zrmFiles.js
+++ force-app/zurich-ventas-app/main/default/lwc/zrmFiles/zrmFiles.js
@@ -2,25 +2,33 @@ import { LightningElement, api, wire } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getRelatedFilesByRecordId from '@salesforce/apex/ZRMFilesController.getRelatedFilesByRecordId';
 import { refreshApex } from '@salesforce/apex';
+import { deleteRecord } from "lightning/uiRecordApi";
+import LightningConfirm from 'lightning/confirm';
 
 // labels
 import ZRM_FilesTitle from "@salesforce/label/c.ZRM_FilesTitle";
-import ZRM_NewFile from "@salesforce/label/c.ZRM_NewFile";
-import ZRM_DownloadFile from "@salesforce/label/c.ZRM_DownloadFile";
-import ZRM_Success from "@salesforce/label/c.ZRM_Success";
-import ZRM_Refresh from "@salesforce/label/c.ZRM_Refresh";
-import ZRM_UploadSuccess from "@salesforce/label/c.ZRM_UploadSuccess";
+import ZRM_Files_NewFile from "@salesforce/label/c.ZRM_Files_NewFile";
+import ZRM_Files_DownloadFile from "@salesforce/label/c.ZRM_Files_DownloadFile";
+import ZRM_Files_Success from "@salesforce/label/c.ZRM_Files_Success";
+import ZRM_Files_Refresh from "@salesforce/label/c.ZRM_Files_Refresh";
+import ZRM_Files_UploadSuccess from "@salesforce/label/c.ZRM_Files_UploadSuccess";
+import ZRM_Files_DeleteConfirmMessage from "@salesforce/label/c.ZRM_Files_DeleteConfirmMessage";
+import ZRM_Files_DeleteConfirmLabel from "@salesforce/label/c.ZRM_Files_DeleteConfirmLabel";
+import ZRM_Files_DeleteConfirmSuccess from "@salesforce/label/c.ZRM_Files_DeleteConfirmSuccess";
 
 export default class ZRMFiles extends LightningElement {
 
     // labels
     label = {
         ZRM_FilesTitle,
-        ZRM_NewFile,
-        ZRM_DownloadFile,
-        ZRM_Success,
-        ZRM_Refresh,
-        ZRM_UploadSuccess
+        ZRM_Files_NewFile,
+        ZRM_Files_DownloadFile,
+        ZRM_Files_Success,
+        ZRM_Files_Refresh,
+        ZRM_Files_UploadSuccess,
+        ZRM_Files_DeleteConfirmMessage,
+        ZRM_Files_DeleteConfirmLabel,
+        ZRM_Files_DeleteConfirmSuccess
     };
 
     @api recordId;
@@ -39,12 +47,33 @@ export default class ZRMFiles extends LightningElement {
         refreshApex(this.filesList);
         // Mostrar mensaje toast
         const uploadedFiles = event.detail.files;
-        this.showToast(this.label.ZRM_Success, uploadedFiles.length + this.label.ZRM_UploadSuccess, 'success');
+        this.showToast(this.label.ZRM_Files_Success, uploadedFiles.length + ' ' + this.label.ZRM_Files_UploadSuccess, 'success');
+    }
+
+    async handleDelete(event) {
+        const recordId = event.target.dataset.id;
+        
+        const result = await LightningConfirm.open({
+            message: this.label.ZRM_Files_DeleteConfirmMessage,
+            variant: 'header',
+            label: this.label.ZRM_Files_DeleteConfirmLabel,
+            theme: 'warning'
+        });
+        if (result) {
+            try {
+                await deleteRecord(recordId);
+                this.showToast(this.label.ZRM_Files_Success, this.label.ZRM_Files_DeleteConfirmSuccess, 'success');
+                await refreshApex(this.filesList);
+
+            } catch (error) {                
+                this.showToast('Error', error.body.message, 'error');
+            }
+        }
     }
 
     refreshComponent() {
         refreshApex(this.filesList);
-        this.showToast(this.label.ZRM_Success, this.label.ZRM_Refresh, 'success');
+        this.showToast(this.label.ZRM_Files_Success, this.label.ZRM_Files_Refresh, 'success');
     }
 
 
