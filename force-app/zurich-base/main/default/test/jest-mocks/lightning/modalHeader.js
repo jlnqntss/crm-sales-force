cbb1eb87319ef927b4c400cd65d3b81aceca47b5
Pitt Olvera <pitt.olvera@seidor.com>
2024-08-27 11:40:53 +0200

    chore: Mocks for lightning modal were updated to include expected vars

--- force-app/zurich-base/main/default/test/jest-mocks/lightning/modalHeader.js
+++ force-app/zurich-base/main/default/test/jest-mocks/lightning/modalHeader.js
@@ -1,3 +1,5 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 
-export default class ModalHeader extends LightningElement {}
+export default class ModalHeader extends LightningElement {
+  @api label;
+}
