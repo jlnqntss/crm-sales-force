608ca631d7e40482f543b5618d15aab198db6c85
Pitt Olvera <pitt.olvera@seidor.com>
2024-09-02 12:42:18 +0200

    chore: Get Tags Token

--- scripts/source/GitLabAPI.js
+++ scripts/source/GitLabAPI.js
@@ -110,7 +110,7 @@ class GitlabAPIService {
       method: "GET",
       headers: {
         "User-Agent": "request",
-        Authorization: `Bearer glpat-ULU5KAxP-9ffJzYxb8GW`,
+        Authorization: "Bearer glpat-ULU5KAxP-9ffJzYxb8GW",
         "Content-Type": "application/json"
       }
     }).then((response) => {
