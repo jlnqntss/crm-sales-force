4283b8e1cd282ec21c72079542f0f345952acafd
NTSMOBILE\mcasanova <mcasanova@seidor.com>
2024-09-03 08:49:52 +0200

    prettify

--- scripts/source/util.js
+++ scripts/source/util.js
@@ -294,14 +294,16 @@ function deploy(deployConfig) {
 
   // 6 - Se ejecuta el despliegue, dependiendo de si se lanza validación o no
   console.log(`[Info] Deploy: Encolando despliegue...`);
-  executeBash(
-    `sf project deploy start ${deployOptions.join(" ")} --json > result.json`, {stdio: []}
+  let result = executeSfdxCommand(
+    `sf project deploy start ${deployOptions.join(" ")}`, 
+    {
+      skipJsonParsing: false,
+      stdio: [],
+    }
   );
   console.log('Parseando resultado...');
-  let deployJob = JSON.parse(
-    fs.readFileSync("result.json", { encoding: "UTF-8" })
-  );
-  console.log(deployJob);
+  let deployJob = JSON.parse(result);
+  //console.log(deployJob);
   const deployResult = deployJob.result;
 
   // 7 - Se guarda el Id. para lanzar posteriormente el Quick Deploy, si aplica
