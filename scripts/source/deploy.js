febc04c1bdb41e165645857e1c5a9e46299169a5
Juan Francisco Herrero Pérez <juanfrancisco.herrero@seidor.com>
2024-10-24 07:12:54 +0000

    feat: OCS-9975 Eliminar formateado en script deploy de pipeline

--- scripts/source/deploy.js
+++ scripts/source/deploy.js
@@ -12,29 +12,29 @@ async function main() {
   const argOffset = isValidation ? 1 : 0;
   const targetOrg = process.argv[2 + argOffset];
   const targetEnvironment = process.argv[3 + argOffset];
-
-  const testRun = process.argv.length > 3 + argOffset;
+  
+  const testRun = process.argv.length > (3 + argOffset);
   const testRunLevel = testRun ? process.argv[4 + argOffset] : undefined;
 
   let target;
   let reconcile = false;
   switch (targetEnvironment) {
     case "prod":
-      if (process.env["CI_FULL_DEPLOYMENT_PROD"] === "true") {
+      if (process.env["CI_FULL_DEPLOYMENT_PROD"] === "true") { 
         let tag = await findLastSemanticTag(null, 1);
         target = tag.target;
       }
-      if (process.env["PROFILE_RECONCILE_PROD"] === "true") {
+      if(process.env["PROFILE_RECONCILE_PROD"] === "true") {
         reconcile = true;
       }
       break;
 
     case "qa":
-      if (process.env["CI_FULL_DEPLOYMENT_QA"] === "true") {
+      if (process.env["CI_FULL_DEPLOYMENT_QA"] === "true") {  
         let tag = await findLastSemanticTag("UAT", 1);
         target = tag.target;
       }
-      if (process.env["PROFILE_RECONCILE_QA"] === "true") {
+      if(process.env["PROFILE_RECONCILE_QA"] === "true") {
         reconcile = true;
       }
       break;
@@ -44,7 +44,7 @@ async function main() {
         let tag = await findLastSemanticTag("rc");
         target = tag.target;
       }
-      if (process.env["PROFILE_RECONCILE_DEV"] === "true") {
+      if(process.env["PROFILE_RECONCILE_DEV"] === "true") {
         reconcile = true;
       }
       break;
@@ -71,6 +71,6 @@ async function main() {
 
 return (async () => {
   await main();
-})().catch((e) => {
+})().catch(e => {
   console.error(e);
 });
